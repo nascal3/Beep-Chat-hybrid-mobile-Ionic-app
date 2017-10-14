9f9ff19f55cf887fb016755c4fde3777848ea06d
nascal3 <nascal3@gmail.com>
2017-10-14 18:34:26 +0300

    created componentes to be placed in the relevant pages

---
+++ src/components/components.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { IonicModule} from 'ionic-angular';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { RegisterFormComponent } from './register-form/register-form.component';
+@NgModule({
+	declarations: [LoginFormComponent,
+    RegisterFormComponent],
+	imports: [IonicModule],
+	exports: [
+	  IonicModule,
+	  LoginFormComponent,
+    RegisterFormComponent
+  ]
+})
+export class ComponentsModule {}
