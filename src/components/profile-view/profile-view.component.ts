4d133e0bd052f783e5aa8f1a2cfc8d99dbd30e7a
nascal3 <nascal3@gmail.com>
2017-10-16 22:29:25 +0300

    profile page complete

---
+++ src/components/profile-view/profile-view.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../providers/data/data.service';
+import {AuthService} from '../../providers/auth/auth.service';
+import {User} from "firebase";
+import {Profile} from "../../models/profile/profile.interfacer";
+import {LoadingController, Loading} from "ionic-angular";
+
+/**
+ * Generated class for the ProfileViewComponent component.
+ *
+ * See https://angular.io/api/core/Component for more info on Angular
+ * Components.
+ */
+@Component({
+  selector: 'app-profile-view',
+  templateUrl: 'profile-view.component.html'
+})
+export class ProfileViewComponent implements OnInit{
+
+ userProfile: Profile;
+ loader: Loading;
+
+  constructor(
+    private data: DataService,
+    private auth: AuthService,
+    private loading: LoadingController
+  ) {
+    this.loader = this.loading.create({content: 'Loading profile...'});
+  }
+
+  ngOnInit() {
+    this.loader.present();
+    this.auth.getAuthenticatedUser().subscribe((user: User) => {
+        this.data.getProfile(user).subscribe(profile => {
+            this.userProfile = <Profile>profile.payload.val();
+            this.loader.dismiss();
+        })
+    });
+  }
+
+}
